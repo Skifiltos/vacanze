efb078992e463feb21a626b5d3b0aae7fe3b23fa
badtype <g.c.faina@gmail.com>
2023-06-06 19:05:03 +0200

    render condizionale

--- src/components/Holiday.js
+++ src/components/Holiday.js
@@ -5,6 +5,7 @@ const url = "https://react--course-api.herokuapp.com/api/v1/data/vacanze";
 
 const Holiday = () => { 
   const [data, setData] = useState([]);
+  const [selected, setSelected] = useState(0);
 
   // funzione pre fetchare dati dall'api
   const getData = async () => {
@@ -19,8 +20,18 @@ const Holiday = () => {
   useEffect(() => {
     getData();
   }, []);
-  
-  return <div>Vacanza</div>;
+
+  // return condizionale per controllare di aver risolto la promise
+  if(data.success) {
+    return <>
+      {
+        // ternary operator per controllare il numero di vacanze
+        data.data.length > 0 ? <SingleHoliday {...data.data[selected]} /> : <h4>No Vacanze</h4>
+      }
+    </>;
+  } else {
+    return <h2>Loading...</h2>;
+  }
 };
 
 export default Holiday;
