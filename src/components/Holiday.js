8b675733f49cb84384d7ed2d6bcc96d34627bcc3
badtype <g.c.faina@gmail.com>
2023-06-06 18:15:12 +0200

    fetch

--- src/components/Holiday.js
+++ src/components/Holiday.js
@@ -3,7 +3,23 @@ import axios from "axios";
 import SingleHoliday from "./SingleHoliday";
 const url = "https://react--course-api.herokuapp.com/api/v1/data/vacanze";
 
-const Holiday = () => {
+const Holiday = () => { 
+  const [data, setData] = useState([]);
+
+  // funzione pre fetchare dati dall'api
+  const getData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  
   return <div>Vacanza</div>;
 };
 
